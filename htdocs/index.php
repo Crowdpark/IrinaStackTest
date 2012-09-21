7c6bac20cdab5f2e401092535c5cfaba428f7d3a
Alexander Finger <a.finger@finger-berlin.de>
2012-09-21 12:38:40 +0200

    new Processus based test app...

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,108 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>FooBar App Example</title>
-    <meta name="description" content="">
-    <meta name="author" content="">
+<pre>
+<?php
+session_start();
 
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+$_SESSION['tracker'] = time();
 
-    <!-- Facebook prefered image source <link href="" rel="image_src"> -->
+var_dump($_SESSION);
 
-    <?php
+?>
+</pre>
+<hr>
+<pre>
+<?php
+$processusCorePath = '../library/Processus/core/';
+$applicationPath   = '../application/php/Application/';
 
-      require('assets.php');
+require_once($processusCorePath . 'Interfaces/InterfaceBootstrap.php');
+require_once($processusCorePath . 'Interfaces/InterfaceApplicationContext.php');
+require_once($processusCorePath . 'ProcessusBootstrap.php');
+require_once($applicationPath . 'ApplicationBootstrap.php');
 
-      $live = ! in_array($_SERVER['HTTP_HOST'], array('localhost', 'local.shakeonitapp.com')) ? TRUE : FALSE;
+$bootstrap = \Application\ApplicationBootstrap::getInstance();
+$bootstrap->init();
 
-      if( ! $live) {
-        foreach($assets['css'] as $file) {
-          echo '<link href="'.$file.'" rel="stylesheet" text="text/css">';
-        }
-      }
-      else {
-          echo join('', file('assets/css/latest-css.bundle'));
-      }
+$context = \Processus\ProcessusContext::getInstance();
 
-    ?>
-
-  </head>
-
-  <body>
-    
-    <div id="fb-root"></div>
-
-    <div id="login-page" class="container">
-      <div class="title row">
-        <h1>FooBar App Example Login</h1>
-      </div>
-
-      <div class="row">
-        <div><fb:login-button data-scope="user_about_me,email">Login with Facebook</fb:login-button></div>
-      </div>
-    </div>
-
-    <div id="matrix">
-
-      <div class="topbar">
-        <div class="topbar-inner">
-          <div class="container">
-            <a href="#!/" class="brand">FooBar App</a>
-            <ul class="nav">
-              <li id="nav-home"><a href="#!/">Home</a></li>
-            </ul>
-
-            <ul class="nav secondary-nav">
-              <li class="dropdown" id="member-container">
-                <a href="#" class="dropdown-toggle"><img src="/assets/img/default_avatar.png"><span></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#!/logout" class="logout">Logout</a></li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-      </div>
-
-      <!-- ######## -->
-
-      <div id="home" class="page container">
-
-        <div class="title row">
-          <h1>Home</h1>
-        </div>
-
-        <div class="row">
-          <div class="left-column span5">
-           LEFT COLUMN
-          </div>
-          <div class="right-column span11">
-           RIGHT COLUMN
-          </div>
-        </div>
-
-      </div>
-
-    </div>
-
-    <?php
-
-      if( ! $live) {
-        foreach($assets['js'] as $file) {
-          echo '<script src="'.$file.'"></script>';
-        }
-      }
-      else {
-          echo join('', file('assets/js/latest-js.bundle'));
-      }
-
-    ?>
-
-    <script type="text/javascript">$(function() { App.Start() })</script>
-
-  </body>
-</html>
+print_r($context);
